d156bfba4b6e44741319f999a0a16f9cb9ff4458
yumelong <yumefuchs@gmail.com>
2023-09-27 15:17:26 +0200

    Web application

--- src/main/java/com/example/springbootwebapp/controller/EmployeeController.java
+++ src/main/java/com/example/springbootwebapp/controller/EmployeeController.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -33,9 +33,17 @@ public class EmployeeController {
         return mav;
     }
 
-    @PostMapping("/saveEmployee")
+    @GetMapping("/saveEmployee")
     public String saveEmployee(@ModelAttribute Employee employee){
         eRepo.save(employee);
         return "redirect:/list";
+    }
+
+    @GetMapping("/showUpdateForm")
+    public ModelAndView showUpdateForm(@RequestParam Long employeeId){
+        ModelAndView mav = new ModelAndView("add-employee-form");
+        Employee employee = eRepo.findById(employeeId).get();
+        mav.addObject("employee", employee);
+        return mav;
     }
 }
